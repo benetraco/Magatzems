73f2d3bd2dfa33175531f23a0a687516ceca3277
benetraco <101699331+benetraco@users.noreply.github.com>
2022-04-10 21:20:56 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -16,23 +16,40 @@ class Strategy: #ERROR! Hi ha algun bucle que no s'acaba
     _log: Logger
     _store: Store
 
+
     def __init__(self, width: int, log_path: str):
         self._log = Logger(log_path, "SimpleStrategy", width)
         self._store = Store(width)
         self._time = 0
     
+    
     def cash(self) -> int:
         return self._store.cash()
 
-    def determine_position(self, cont_size: int) -> int:
-        """Returns the position of the first stack"""
-        assert cont_size <= 4
-        if cont_size == 1: return 0
-        elif cont_size == 2: return 2
-        elif cont_size == 3: return 6
-        else: return 12 #cont_size == 4
     
-    def determine_next_position(self, p: Position) -> int:
+    def time(self) -> int:
+        return self._time
+
+
+    def update_time(self) -> None:
+        self._time += 1
+
+
+    def det_position_first_container(self, c_size: int) -> int:
+        """Returns the position that has to go the container that arrives 
+        to the store based on its size"""
+
+        assert c_size <= 4
+        if c_size == 1: return 0
+        elif c_size == 2: return 2
+        elif c_size == 3: return 6
+        else: return 12 #c_size == 4
+    
+
+    def det_next_position(self, p: Position) -> int:
+        """Returns the next position in which the program has to 
+        operate based on the position it is operating"""
+
         if p == 0: return 1
         elif p == 1: return 2
         elif p == 2: return 4
@@ -42,62 +59,124 @@ class Strategy: #ERROR! Hi ha algun bucle que no s'acaba
         elif p == 12: return 16
         else: return 0 #p == 16
 
-    def expired(self, c: Container, t: int) -> bool:
-        """Returs if a container is expired at that moment"""
-        return t >= c.delivery.end
 
-    def can_deliver(self, c: Container, t: int) -> bool:
-        """Returns if a container can be delivered at that moment"""
-        return c.delivery.start <= t and t < c.delivery.end
+    def add_first_container(self, c: Container) -> None:
+        """Adds the continer that arrives to the store the appropiate position
+        The action is added to the logger."""
 
-    def move_continer_pila(self, c: Container, p: Position, t: int, next: bool) -> None:
-        """
-        Moves a continer to the next stack of it's size if bool "next" is True or to the stack 
-        before if the bool "next" is False unless it can be removed from the store
-        """
-        if self.expired(c, t):
-            self._store.remove(c)
-            self._log.remove(t, c)
-            self._log.cash(t, self._store.cash())
-        elif self.can_deliver(c, t):
-            self._store.add_cash(c.value)
-            self._log.cash(t, self._store.cash())
-            self._store.remove(c)
-            self._log.remove(t, c)
-        else: #the top container of the stack cannot be delivered neither is expired
-            if next:
-                self._store.move(c, p + c.size)
-                self._log.move(t, c, p + c.size)
-            else:
-                self._store.move(c, p - c.size)
-                self._log.move(t, c, p - c.size)
-            
-    def exec(self, c: Container): #ERROR! Hi ha algun bucle que no s'acaba
-        """Apliquem la excecució simple"""
-        assert self._store.width() >= 20
-        self._time = c.arrival.start
-        p = self.determine_position(c.size)
+        p = self.det_position_first_container(c.size)
         self._store.add(c, p)
         self._log.add(self._time, c, p)
-        self._time += 1
-        stack = 0
-        while self._time < c.arrival.end:
-            while self._time < c.arrival.end and stack < self._store.width():
-                if self._store.empty():
-                    self._time += 1 #c.arrival.end
-                else:
-                    cont = self._store.top_container(stack)
-                    while cont is not None and self._time < c.arrival.end:
-                        self.move_continer_pila(cont, stack, self._time, True) #we write the bool True because we want to move the continer to the next stack
-                        self._time += 1
-                        cont = self._store.top_container(stack)
-                    stack = self.determine_next_position(stack)
-                    cont = self._store.top_container(stack)
-                    while cont is not None and self._time < c.arrival.end:
-                        self.move_continer_pila(cont, stack, self._time, False) #we write the bool False because we want to move the continer to the stack before
-                        self._time += 1
-                        cont = self._store.top_container(stack)
-                    stack = self.determine_next_position(stack) #determines the next stack in which we have to operate based in the stack in which we are
+        self.update_time()
+
+
+    def expired(self, c: Container) -> bool:
+        """Returs if a container is expired at that moment"""
+
+        return self.time() >= c.delivery.end
+
+
+    def deliverable(self, c: Container) -> bool:
+        """Returns if a container is deliverable at that moment"""
+
+        return c.delivery.start <= self.time() and self.time() < c.delivery.end
+
+
+    def remove_expired(self, c: Container) -> None:
+        """
+        Removes the continer c from the store without adding it's cash to the store.
+        The actions are added to the logger.
+
+        Pre: c is expired
+        """
+
+        self._store.remove(c)
+        self._log.remove(self.time(), c)
+        self._log.cash(self.time(), self.cash())
+    
+
+    def remove_deliverable(self, c: Container) -> None:
+        """
+        Removes de continer c from the store adding it's value to the store's cash.
+        The actions are added to the logger.
+
+        Pre: c is deliverable
+        """
+
+        self._store.add_cash(c.value)
+        self._store.remove(c)
+        self._log.remove(self.time(), c)
+        self._log.cash(self.time(), self.cash())
+    
+
+    def move_next_or_before(self, c: Container, p: Position, second: bool) -> None:
+        """
+        Moves the contanier c to the second stack or the the first stack
+        of it's size depending on the bool "second".
+        The action is added to the logger.
+
+        Pre: c can be moved because is the top continer of the position of it's size
+        """
+
+        if second:
+            self._store.move(c, p + c.size)
+            self._log.move(self.time(), c, p + c.size)
+        else:
+            self._store.move(c, p - c.size)
+            self._log.move(self.time(), c, p - c.size)
+
+
+    def move_container_pila(self, c: Container, p: Position, second: bool) -> None:
+        """
+        If the container is explired or can be delivered it is removed from the 
+        container properly. If not it is moved to the second stack or the first 
+        stack of it's size depending on the bool "second".
+        
+        Pre: c can be removed because is the top continer of the position of it's size
+        """
+
+        if self.expired(c):
+            self.remove_expired(c)
+
+        elif self.deliverable(c):
+            self.remove_deliverable(c)
+
+        else: #the top container of the position cannot be delivered neither is expired
+            self.move_next_or_before(c, p, second)
+        
+        self.update_time()
+            
+
+    def exec(self, c: Container):
+        """Apliquem l'excecució simple"""
+
+        assert self._store.width() >= 20 
+        #we make sure that the with of the store is 20 (if is smaller we cannot apply the simple strategy)
+
+        self.add_first_container(c)
+        p = 0
+        #after we do an action we have to stop the algorithm if the next container arrives
+        while self._time < c.arrival.end: 
+            
+            if self._store.empty(): #if the store is empty we don't have to do anything until the next container arrives
+                self._time = c.arrival.end
+
+            else: #until the position is empty we get all the containers from top to the bottom and follow the simple algorithm
+                
+                top_c = self._store.top_container(p)
+
+                while top_c is not None and self._time < c.arrival.end: #we move all the containers from the first stack of the containers' size to the second
+                    self.move_container_pila(top_c, p, True) #we write the bool True because we want to move the container second stack of it's size
+                    top_c = self._store.top_container(p)
+
+                p = self.det_next_position(p) #determines the next position we have to operate based in the position we are
+                top_c = self._store.top_container(p)
+
+                while top_c is not None and self._time < c.arrival.end: #we move all the containers from the second stack of the containers' size to the first
+                    self.move_container_pila(top_c, p, False) #we write the bool False because we want to move the container first stack of it's size
+                    top_c = self._store.top_container(p)
+
+                p = self.det_next_position(p) #determines the next position we have to operate based in the position we are
 
 
 def init_curses():
@@ -128,9 +207,7 @@ def main(stdscr: curses.window):
     log_path = sys.argv[2]
     width = int(sys.argv[3])
 
-    #print("h")
     execute_strategy(containers_path, log_path, width)
-    #print("EXECUTED")
     check_and_show(containers_path, log_path, stdscr)
 
 
